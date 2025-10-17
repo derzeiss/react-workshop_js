55828e90300883d8910013d4dc6fda74b376bc7b
derzeiss <derzeiss@users.noreply.github.com>
2025-10-17 17:21:02 +0200

    05 Spread syntax and array functions

--- src/main.js
+++ src/main.js
@@ -5,13 +5,27 @@ const PERSONS = [
   { name: "Jane Dae", bio: "Living on the road in my minivan writing code" },
 ];
 
+const POSTS = [
+  { title: "React is awesome", author: "John Doe" },
+  { title: "Why you need to learn JS now", author: "Jane Dae" },
+  { title: "React 19 is here!", author: "John Doe" },
+];
+
+const PERSONS_WITH_POSTS = PERSONS.map((person) => ({
+  ...person,
+  posts: POSTS.filter((post) => post.author === person.name),
+}));
+
 const render = (component, selector = "#app") => {
   document.querySelector(selector).innerHTML = component();
 };
 
-const person = ({ name, bio }) => {
+const person = ({ name, bio, posts }) => {
   return `<h2>${name}</h2>
   <p>${bio}</p>
+  <ul>
+    ${posts.map((post) => `<li>${post.title}</li>`).join("\n")}
+  </ul>
   `;
 };
 
@@ -19,4 +33,4 @@ const personList = (persons) => {
   return persons.map((p) => person(p)).join("\n");
 };
 
-render(() => personList(PERSONS));
+render(() => personList(PERSONS_WITH_POSTS));
